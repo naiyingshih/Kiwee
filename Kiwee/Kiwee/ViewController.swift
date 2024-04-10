c76b4f345f6a43054a59534aef32b6c89c48ae4f
NY <ib310519@gmail.com>
2024-04-10 18:05:01 +0800

    create new project Kiwee

---
+++ Kiwee/Kiwee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kiwee
+//
+//  Created by NY on 2024/4/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
