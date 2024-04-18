5933ea788c321fb36c5a5785855af6c72a28aafa
NY <ib310519@gmail.com>
2024-04-19 02:13:31 +0800

    Home page UI: create farm scene scroll view and buttons

---
+++ Kiwee/Kiwee/View/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Kiwee
+//
+//  Created by NY on 2024/4/19.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
